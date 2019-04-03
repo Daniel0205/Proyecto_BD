ab9ea276db39bb8a32d561c62156f5d74cb499e6
jempool <jem.suarez@correounivalle.edu.co>
2019-04-03 13:58:01 -0500

    añadido al login envio de json cliente <-> servidor

--- Cliente/src/components/Login.js
+++ Cliente/src/components/Login.js
@@ -1,51 +1,76 @@
-import React, { Component } from 'react';
-
-import './Login.css'
+import React, { Component } from "react";
+import "./Login.css";
 
 
 class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      str: [],
+      cellphone: "",
+      psw: ""
     };
-    
     this.handleClick = this.handleClick.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleClick(){
-    this.props.callback('Registro');    
+
+  handleClick() {
+    this.props.callback("Registro");
   }
+
 
   handleSubmit(event) {
-    fetch('/hola')
-    .then(res => res.json())
-    .then(users => this.setState({str:users}))
-    
-    console.log(this.state.str)
-    event.preventDefault()     
+    event.preventDefault();
+    const { cellphone, psw } = this.state;
+    fetch("/hola", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ Username: cellphone, Password: psw })
+    })
+      .then(res => res.json())
+      .then(res => console.log(res));
   }
 
-  render() {
-    return (
-      <div className='login'>
+//Funcion para actualizar las variables ingresadas en el login
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
 
-        <form className="form" name='login' onSubmit={this.handleSubmit}>
-            <h1>Login</h1>
-            <p>Username:</p>
-            <input type="text" name="cellphone" placeholder='Numero de telefono'/>
-            <p>Password:</p>
-            <input type="password" name="psw" placeholder='Password'/><br></br>
-            <input type="submit" name="psw" value='Ingresar'/>
-            
-        </form> 
-        <div className='registro'>
+
+  render() {
+    const { cellphone, psw } = this.state;
+    return (
+      <div className="login">
+        <form className="form" name="login" onSubmit={this.handleSubmit}>
+          <h1>Login</h1>
+          <p>Username:</p>
+          <input
+            type="text"
+            name="cellphone"
+            placeholder="Numero de telefono"
+            value={cellphone}
+            onChange={this.onChange}
+          />
+          <p>Password:</p>
+          <input
+            type="password"
+            name="psw"
+            placeholder="Password"
+            value={psw}
+            onChange={this.onChange}
+          />
+          <br />
+          <input type="submit" name="submit" value="Ingresar" />
+        </form>
+        <div className="registro">
           <p>No tienes una cuenta?</p>
-          <button onClick={this.handleClick} >Regristrarse</button>
+          <button onClick={this.handleClick}>Regristrarse</button>
         </div>
       </div>
-    ); 
+    );
   }
 }
 export default Login;
--- Servidor/app.js
+++ Servidor/app.js
@@ -1,13 +1,19 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 
-app.get('/hola', function (req, res) {
-  res.json([
-      {id:1,username:"somebody"},
-      {id:2,username:"other somebody"}
-  ])
+var bodyParser = require("body-parser"); // middleware  to handle HTTP POST request
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
+
+
+app.post("/hola", function (req, res) {
+  if (req.body.Username == "admin" && req.body.Password == "admin") {
+    res.json([{ usuario: "admin", login: true }]);
+  }
+  console.log(req.body)
 });
 
+
 app.listen(3001, function () {
-  console.log('Example app listening on port 3001!');
+  console.log("Example app listening on port 3001!");
 });
